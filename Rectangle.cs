218b7a30c8d13759280d5a43a21ab3602fb8d1f5
Valentyn <145349837+Rongarst@users.noreply.github.com>
2023-11-23 17:22:23 +0200

    first commit

---
+++ Rectangle.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+class Rectangle
+{
+    private double side1;
+    private double side2;
+    public Rectangle(double side1, double side2)
+    {
+        this.side1 = side1;
+        this.side2 = side2;
+    }
+    public double CalculateArea()
+    {
+        return side1 * side2;
+    }
+    public double CalculatePerimeter()
+    {
+        return 2 * (side1 + side2);
+    }
+    public double Area
+    {
+        get { return CalculateArea(); }
+    }
+    public double Perimeter
+    {
+        get { return CalculatePerimeter(); }
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введіть довжину першої сторони прямокутника:");
+        double side1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введіть довжину другої сторони прямокутника:");
+        double side2 = Convert.ToDouble(Console.ReadLine());
+
+        Rectangle rect = new Rectangle(side1, side2);
+
+        Console.WriteLine($"Площа прямокутника: {rect.Area}");
+        Console.WriteLine($"Периметр прямокутника: {rect.Perimeter}");
+
+        Console.ReadLine();
+    }
+}
